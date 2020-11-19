a3f5dac9c652ccdfadb3f0540e3b10fadec2f6ef
Pisitpong122 <73678469+Pisitpong122@users.noreply.github.com>
2020-11-19 14:30:05 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,38 +1,50 @@
 <html>
+
 <head>
-<title>LabITF</title>
+    <title>LabITF</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'labmysql.mysql.database.azure.com', 'it63070122@labmysql', 'Ppp192544', 'LabITF', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
+    <?php
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, 'labmysql.mysql.database.azure.com', 'it63070122@labmysql', 'Ppp192544', 'LabITF', 3306);
+    if (mysqli_connect_errno($conn)) {
+        die('Failed to connect to MySQL: ' . mysqli_connect_error());
+    }
+    $res = mysqli_query($conn, 'SELECT * FROM guestbook');
+    ?>
+    <table width="600" border="1">
+        <tr>
+            <th width="100">
+                <div align="center">Name</div>
+            </th>
+            <th width="350">
+                <div align="center">Comment </div>
+            </th>
+            <th width="150">
+                <div align="center">Action </div>
+            </th>
+        </tr>
+        <?php
+        while ($Result = mysqli_fetch_array($res)) {
+        ?>
+            <tr>
+                <td><?php echo $Result['name']; ?></div></td>
+                <td><?php echo $Result['comment']; ?></td>
+                <td><button type="button" class="btn btn-warning">Warning</button></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <?php
+    mysqli_close($conn);
+    ?>
 </body>
+
 </html>
